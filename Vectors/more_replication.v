cae6d7ec0a9e0b853235f93a2d8f4a4782d7f5e7
saisharan2k3 <saisharan2k3@gmail.com>
2026-04-17 23:40:14 +0530

    Create more_replication.v

---
+++ Vectors/more_replication.v
@@ -0,0 +1,9 @@
+module top_module (
+    input a, b, c, d, e,
+    output [24:0] out );//
+    wire [24:0] conc1;
+    wire [24:0] conc2;
+    assign conc1={{5{a}},{5{b}},{5{c}},{5{d}},{5{e}}};
+    assign conc2={5{a,b,c,d,e}};
+    assign out=~(conc1^conc2);
+endmodule
