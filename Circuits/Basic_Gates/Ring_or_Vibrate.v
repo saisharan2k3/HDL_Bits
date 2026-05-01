bf6895f6109c62fe20f25f161e09e04b66702612
saisharan2k3 <saisharan2k3@gmail.com>
2026-05-01 16:35:39 +0530

    Create Ring_or_Vibrate.v

---
+++ Circuits/Basic_Gates/Ring_or_Vibrate.v
@@ -0,0 +1,10 @@
+module top_module (
+    input ring,
+    input vibrate_mode,
+    output ringer,       // Make sound
+    output motor         // Vibrate
+);
+    assign motor = vibrate_mode&ring ? 1 : 0;
+    assign ringer = ~vibrate_mode&ring ? 1 : 0;
+
+endmodule
