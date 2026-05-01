98c7da7b821b68f690b9a1515d126b0c86f6410f
saisharan2k3 <saisharan2k3@gmail.com>
2026-05-01 16:36:13 +0530

    Create Thermostat.v

---
+++ Circuits/Basic_Gates/Thermostat.v
@@ -0,0 +1,13 @@
+module top_module (
+    input too_cold,
+    input too_hot,
+    input mode,
+    input fan_on,
+    output heater,
+    output aircon,
+    output fan
+); 
+    assign heater = mode&too_cold ? 1 : 0;
+    assign aircon = ~mode&too_hot ? 1 : 0;
+    assign fan = fan_on|heater|aircon;
+endmodule
