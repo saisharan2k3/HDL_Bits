40ef02e120553c291f4f57059dd0d5ca326547a1
saisharan2k3 <saisharan2k3@gmail.com>
2026-05-04 22:28:18 +0530

    Delete Modules directory

--- Modules/Start.v
+++
@@ -1,3 +0,0 @@
-module top_module ( input a, input b, output out );
-    mod_a inst(.in1(a),.in2(b),.out(out));
-endmodule
--- Modules/Three_Modules.v
+++
@@ -1,6 +0,0 @@
-module top_module ( input clk, input d, output q );
-wire q0,q1;
-    my_dff inst0(clk,d,q0);
-    my_dff inst1(clk,q0,q1);
-    my_dff inst2(clk,q1,q);
-endmodule
